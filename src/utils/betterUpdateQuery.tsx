442bb42d5af88615e7a8f06ee4e982716f9044ce
amarjupalli <ajupalli@turo.com>
2020-10-24 19:12:28 -0700

    refactor: move createUrqlclient and betterUpdateQuery into utils; setup ssr in _app.js

---
+++ src/utils/betterUpdateQuery.tsx
@@ -0,0 +1,11 @@
+import { Cache, QueryInput } from "@urql/exchange-graphcache";
+
+// from @benawad's course
+export function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
