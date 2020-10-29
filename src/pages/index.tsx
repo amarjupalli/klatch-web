ce8498c847998810a224f1724caa5ba258c76c32
amarjupalli <ajupalli@turo.com>
2020-10-28 20:47:01 -0700

    feat: first pass for load more pagination fn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import { withUrqlClient } from "next-urql";
 import NextLink from "next/link";
-import { usePostsQuery } from "../generated/graphql";
+import { Post, usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { Layout } from "../components/Layout";
-import { Divider, Icon, Link } from "@chakra-ui/core";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Icon,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/core";
+
+interface VariablesTypes {
+  limit: number;
+  cursor: null | string;
+}
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+  const [variables, setVariables] = useState<VariablesTypes>({
+    limit: 10,
+    cursor: null,
+  });
+  const [{ fetching, data }] = usePostsQuery({ variables });
+
+  const onShowMoreClick = (lastPostCreatedAt: string) => {
+    const cursor = new Date(parseInt(lastPostCreatedAt)).toUTCString();
+    return setVariables({ limit: variables.limit, cursor });
+  };
 
   return (
     <Layout>
-      <NextLink href="/create-post">
-        <Link color="orange.600">
-          <Icon name="edit" size="24px" /> create a post
-        </Link>
-      </NextLink>
-      <Divider borderColor="black.200" width="50%" />
+      <Flex>
+        <Heading fontSize="xl" color="red.600">
+          posts
+        </Heading>
+        <NextLink href="/create-post">
+          <Link ml="auto" color="orange.600">
+            <Icon name="edit" size="24px" /> create a new post
+          </Link>
+        </NextLink>
+      </Flex>
       {data ? (
-        data.posts.map((d) => <div key={d.id}>{d.title}</div>)
+        <>
+          <Stack spacing={8} mt={4}>
+            {data.posts.map(({ id, title, textSnippet }) => (
+              <Box
+                key={id}
+                p={5}
+                borderWidth="1px"
+                borderColor="orange.200"
+                shadow="md"
+              >
+                <NextLink href={`/post/${id}`}>
+                  <Heading as={Link} fontSize="xl">
+                    {title}
+                  </Heading>
+                </NextLink>
+                <Text mt={4}>{`${textSnippet}...`}</Text>
+              </Box>
+            ))}
+          </Stack>
+          <Flex mt={2}>
+            <Button
+              isLoading={fetching}
+              mt={2}
+              variant="ghost"
+              variantColor="orange"
+              m="auto"
+              onClick={() =>
+                onShowMoreClick(data.posts[data.posts.length - 1].createdAt)
+              }
+            >
+              Show more
+            </Button>
+          </Flex>
+        </>
       ) : (
-        <div>"loading..."</div>
+        <div>
+          {fetching && "loading...please wait"}
+          {!fetching && "No posts to show."}
+        </div>
       )}
     </Layout>
   );
