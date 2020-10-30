49766e769019cdbab3d6977416b6fc544f88e166
amarjupalli <ajupalli@turo.com>
2020-10-30 00:20:10 -0700

    fix: caching for newly created posts

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -72,6 +72,15 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          createPost: (_result, _args, cache, _info) => {
+            const allFields = cache.inspectFields("Query");
+            const fieldInfos = allFields.filter(
+              ({ fieldName }) => fieldName === "posts"
+            );
+            fieldInfos.forEach((field) => {
+              cache.invalidate("Query", "posts", field.arguments || {});
+            });
+          },
           logout: (_result, _args, cache, _info) => {
             betterUpdateQuery<LogoutMutation, MeQuery>(
               cache,
